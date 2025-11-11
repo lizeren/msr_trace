4478b7bbba0c689331599893cbdedf145e1db58c
lizeren <li3103@purdue.edu>
2025-11-10 21:05:30 -0500

    update tests for llvm insertion

--- pmc-insert/test/simple_test_ref.c
+++ pmc-insert/test/simple_test_ref.c
@@ -15,18 +15,28 @@ int helper_func(int x) {
 }
 
 int main() {
+    int a = 0;
     // Test case 1: Expression statement
     addition(5, 3);
     
     // Test case 2: Assignment statement
+    a = subtraction(10, 5);
+
+    // Test case 3: Declaration initializer
     int sum = addition(10, 20);
     
-    // Test case 3: From helper function
+    // Test case 4: From helper function (not a target function)
     int h = helper_func(7);
     
-    // Test case 4: Multiple calls in main
+    // Test case 5: Multiple target functions in rows
     subtraction(100, 50);
     addition(1, 2);
+
+    // TODO:
+    // if(addition(1,2) == subtraction(5,2)){
+    //     // do nothing
+    // }
+
+    return addition(1,2);
     
-    return 0;
 }
